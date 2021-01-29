bbaf0f0906a3615f5a0bbf888b32bb0a4b6d34d1
chanibori <kimchan0765@gmail.com>
2021-01-30 01:40:05 +0900

    update

--- books/python/처음 시작하는 Python(2판)/chapter5/alignment.py
+++ books/python/처음 시작하는 Python(2판)/chapter5/alignment.py
@@ -1,2 +1,13 @@
 # 정렬
 
+# 1. 30으로 지정한 공간내에서 가운데 정렬
+# >>> setup
+# 'a duck goees into a bar...'
+# >>> setup.center(30)
+# '  a duck goees into a bar...  '
+
+# 2. 30으로 지정한 공간내에서 왼쪽 정렬, 오른쪽 정렬
+# >>> setup.ljust(30)
+# 'a duck goees into a bar...    '
+# >>> setup.rjust(30)
+# '    a duck goees into a bar...'
